454967dcce7443f9c22a622b16e9cfef9076ceea
Yusuke Sangenya <longinus.eva@gmail.com>
2019-02-17 22:42:42 +0900

    flush output dir

--- pamphlet-converter/build.sh
+++ pamphlet-converter/build.sh
@@ -4,4 +4,4 @@ cat pamphlet2019.tex | stack run output
 mkdir -p output/images
 mkdir -p output/static
 cp static/all.css output/static
-#ls .. | grep pdf | grep -v pamphlet | xargs -I{} convert ../{} output/images/{}.png
+ls .. | grep pdf | grep -v pamphlet | xargs -I{} convert ../{} output/images/{}.png
