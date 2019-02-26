5a9345d2882f1672779ec9a4b25a405eeca041fc
Yusuke Sangenya <longinus.eva@gmail.com>
2019-02-27 00:49:52 +0900

    preload image

--- pamphlet-converter/static/slide.js
+++ pamphlet-converter/static/slide.js
@@ -1,5 +1,8 @@
 (function() {
     const max = 7;
+    for(var i = 0; i< max; i++) {
+        $('<img>').attr('src', 'slide' + i + '.JPG');
+    }
     const interval = 5000;
     var cnt = 0;
     setInterval(function() {
